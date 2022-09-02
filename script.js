697475599e3b6f30bdfcea71ff5534a6707c280b
devendra <devyadav3001@gmail.com>
2022-09-02 17:09:07 +0530

    radhe radhe

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 // api url
-const api_url = "https://api.github.com/users";
 
+const api_url = "https://api.github.com/users";
 var git = document.getElementById("searchUser")
 // Defining async function
 async function getapi(e) {
     // Storing response
     const response = await fetch(api_url)
-
     // Storing data in form of JSON
     var data = await response.json();
-
     return data
 }
 var a = getapi()
@@ -18,13 +16,22 @@ a.then((val) => {
 })
 async function getdata(fil) {
     var git = document.getElementById("searchUser")
-
-    git.addEventListener('keyup', (e) => {
+    git.addEventListener('input', (e) => {
         let found = fil.filter(item => !item["login"].indexOf(e.target.value))
+        let name = found[0].login;
         document.getElementById("proPic").src = found[0].avatar_url
-        document.getElementById("userName").innerHTML = found[0].login
-        console.log(found[0].followers_url);
+        document.getElementById("userName").innerHTML = name
+     
         localStorage.setItem("key", JSON.stringify(found[0].login));
-
+// followers
+        fetch(`https://api.github.com/users/${name}`)
+        .then(resp => resp.json())
+        .then(res => {
+        document.getElementById("gitFollowers").textContent=res.followers
+        document.getElementById("gitFollowing").textContent=res.following
+        document.getElementById("gitRepos").textContent=res.public_repos
+        
+        })
     })
+
 }
